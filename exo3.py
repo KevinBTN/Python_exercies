c4432d1d562da3b5d0d2d07d4d2f87b09e4a245c
Debian Live user <amnesia@localhost>
2017-09-04 20:11:30 +0000

    exo complet affiche les nombres sup au chiffre entré

--- exo3.py
+++ exo3.py
@@ -1,6 +1,14 @@
 a  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-for element in a:
-    if element < 5:
-        print(element)
+b = []
+
+c = input("type the highest number")
+
+for i in a:
+
+    if i < int(c):
+        b.append(i)
+       
+print(b)
+
 
