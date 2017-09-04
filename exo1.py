829f80e108cd20d1a2d3d90a48191d1fc256d1ca
Debian Live user <amnesia@localhost>
2017-09-04 15:25:32 +0000

    affiche lanné des 100 ans

---
+++ exo1.py
@@ -0,0 +1,16 @@
+NameUser = input("What/'s your name? ")
+
+print("Your name is " + NameUser)
+
+AgeUser = input("How old are you? ")
+
+Année_actuel = 100
+
+
+Diff = int(Année_actuel) - int(AgeUser)
+
+
+Year = int(2017 + Diff) 
+
+print("you will be 100 years old on the year " + str(Year))
+
